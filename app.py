f817f67e6f898ebe97ff86e8708d8b8efa4cec16
RD <rupindeeplearning@gmail.com>
2021-03-09 11:13:32 -0500

    Updated runtime.txt

--- app.py
+++ app.py
@@ -71,9 +71,9 @@ def processRequest(req):
     #else:
     #    log.write_log(sessionID, "Bot Says: " + result.fulfillmentText)
 
-if __name__ == '__main__':
-    app.run()
 #if __name__ == '__main__':
-#    port = int(os.getenv('PORT', 5000))
-#    print("Starting app on port %d" % port)
-#    app.run(debug=False, port=port, host='0.0.0.0')
+    #app.run()
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
