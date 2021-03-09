ec0c6729eac8ebee433ee56ee4c392c30a24800b
RD <rupindeeplearning@gmail.com>
2021-03-09 11:25:02 -0500

    Updated runtime.txt

--- app.py
+++ app.py
@@ -75,6 +75,6 @@ def processRequest(req):
 #if __name__ == '__main__':
     #app.run()
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 33507))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
